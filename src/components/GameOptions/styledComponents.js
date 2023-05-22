9bec7fdfc3f1173e4c935c76deebb13ee2534bc4
Anudeepthi <deepthianu409@gmail.com>
2023-05-22 09:06:16 +0000

    rock paper scissors game in react

---
+++ src/components/GameOptions/styledComponents.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const OptionImage = styled.img`
+  width: 168px;
+  height: 168px;
+`
+export const OptionListItem = styled.li`
+  list-style-type: none;
+  display: flex;
+  margin: 15px;
+`
+export const GameOptionButton = styled.button`
+  outline: none;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+`
